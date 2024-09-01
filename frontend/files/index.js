e941a813918d87b36020dc9a51d359590b9125d1
Samarpan <samarpansmacbookpro@Samarpans-MacBook-Pro.local>
2024-09-01 09:21:17 +0530

    changed some logics

--- frontend/files/index.js
+++ frontend/files/index.js
@@ -12,6 +12,27 @@
 //     console.log(`Server is running on port ${port}`);
 // });
 
+
+
+// const express = require("express");
+// const path = require("path");
+
+// const app = express();
+// const port = 3000;
+
+// // Serve static files from the 'public' directory
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "index.html"));
+// });
+
+// app.listen(port, function() {
+//     console.log(`Server is running on port ${port}`);
+// });
+
+
+
 const express = require("express");
 const path = require("path");
 
@@ -25,6 +46,12 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// Serve the c-appointment page after login
+app.get("/c-appointment", function(req, res) {
+    res.sendFile(path.join(__dirname, "c-appointment.html"));
+});
+
 app.listen(port, function() {
     console.log(`Server is running on port ${port}`);
 });
+
